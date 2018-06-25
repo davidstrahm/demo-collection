21c08d1ce45762bae8b4db416d7b288e41e12635
David Strahm <strahmdavid@gmail.com>
2018-06-25 09:00:00 +0200

    Update gitignore, added js files

---
+++ src/my-other-schematic/index_spec.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const schematics_1 = require("@angular-devkit/schematics");
+const testing_1 = require("@angular-devkit/schematics/testing");
+const path = require("path");
+const collectionPath = path.join(__dirname, '../collection.json');
+describe('my-other-schematic', () => {
+    it('works', () => {
+        const runner = new testing_1.SchematicTestRunner('schematics', collectionPath);
+        const tree = runner.runSchematic('my-other-schematic', {}, schematics_1.Tree.empty());
+        expect(tree.files.sort()).toEqual(['/allo', '/hola']);
+    });
+});
+//# sourceMappingURL=index_spec.js.map
